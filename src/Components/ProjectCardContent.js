d3df7839bd96d51653c9fbc6cbfa8bcd423ed2d7
Ofure Alakhume <alakhume.ofure@gmail.com>
2022-06-29 16:13:03 -0400

    component update
    updated the style for project cards

--- src/Components/ProjectCardContent.js
+++ src/Components/ProjectCardContent.js
@@ -10,22 +10,17 @@ const ProjectCardContent = (props) => {
                 <img src={props.imgsrc} alt="react weather app" />
             </div>
             <div className="body-content">
-                <div className="details">
-                    <h2 className="project-title">
-                        {props.title}
-                    </h2>
-                    <p>{props.text}</p>
 
-                    <div className="project-info">
-                        <div className="project-lang">
-                            React
-                        </div>
-                        <button className='btn project-btns'>
-                            <a href={props.view} target="_blank" rel='noreferrer' >View</a>
-                        </button>
+                <h2 className="project-title">
+                    {props.title}
+                </h2>
+                <p>{props.text}</p>
 
-                    </div>
-                </div>
+
+                <button className='btn project-btns'>
+                    <a href={props.view} target="_blank" rel='noreferrer' >View</a>
+                </button>
+
             </div>
         </div>
     )
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ProjectCards from './ProjectCards'
 
 const Projects = () => {
- return (
-  <section className='section-container'>
-   <div className="content-container">
-    <h1>A couple of things i've worked on</h1>
-    <ProjectCards />
-   </div>
-  </section>
- )
+    return (
+        <section className='section-container'>
+            <div className="content-container">
+                <h1>Projects</h1>
+                <ProjectCards />
+            </div>
+        </section>
+    )
 }
 
 export default Projects
